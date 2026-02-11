145d34ae677960e064811c5ae370ec6d2aaab697
GuTaeho <sbpdt1062@gmail.com>
2026-02-12 00:23:41 +0900

    Fix: view.config 운영 및 개발 분기 추가

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/release/web-playground-react/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/release/web-playground-react/' : '/',
   plugins: [react()],
-})
+}))
